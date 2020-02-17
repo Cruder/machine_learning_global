fab1ad5fee47e7eece77aff64ac24f0662d1e7f7
amelie-certin <amelie.certin@gmail.com>
2020-02-17 18:06:49 +0100

    Add missing files

--- matrix/src/matrix.cr
+++ matrix/src/matrix.cr
@@ -12,8 +12,13 @@ end
 
 class TronLinearModel
   @model: LibNeuratron::LinearModel
+
   def initialize(inputs : Array(Float64))
     @model = LibNeuratron.create_linear_model(inputs, inputs.size).value
+  end
+
+  def train
+    LibNeuratron.train_linear_model(10)
   end
 end
 inputs = [  0.0, 0.0,
@@ -22,4 +27,4 @@ inputs = [  0.0, 0.0,
             0.0, 1.0  ]
 expected_outputs = [0, 0, 1, 1]
 model = TronLinearModel.new(inputs)
-# pp LibLibrary.train_linear_model(10)
+pp model.train
