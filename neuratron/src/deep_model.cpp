efec3364fd9f690266317c8a88ddcf40e02be99e
Barogthor <f.thery3103@gmail.com>
2020-02-18 12:02:32 +0100

    DeepModel C++ init

---
+++ neuratron/src/deep_model.cpp
@@ -0,0 +1,33 @@
+#include "neuratron.h"
+#include <stdlib.h>
+#include <Eigen/Dense>
+
+extern "C"{
+    struct DeepModel* create_deep_model(int* neurons_per_layers, int size){
+        struct DeepModel* model = new DeepModel;
+        model->d = new int[size];
+        model->deltas = new double*[size];
+        model->x = new double*[size];
+        model->w = new double**[size-1];
+        for(int i = 0 ; i < size-1 ; i++) {
+            const int size_layer = neurons_per_layers[i] + 1;
+            model->d[i] = size_layer;
+            model->deltas[i] = new double[size_layer];
+            model->w[i] = new double *[size_layer];
+            for (int j = 0; j < size_layer; j++) {
+                model->w[i][j] = new double[neurons_per_layers[i + 1]];
+                for (int k = 0; k < neurons_per_layers[i + 1]; k++)
+                    model->w[i][j][k] = 0;
+                model->x[i] = new double[size_layer];
+            }
+        }
+        const int size_last_layer = neurons_per_layers[size - 1];
+        model->d[size - 1] = size_last_layer;
+        model->deltas[size - 1] = new double[size_last_layer];
+        for (int j = 0; j < size_last_layer; j++) {
+            model->x[j] = new double[size_last_layer + 1];
+
+            return model;
+        }
+    }
+}
