e1ad5c700b7bc2d0a35e6d3f0566b57b752f09ff
Alexandre Lairan <lairana@free.fr>
2020-02-19 20:10:34 +0100

    Matrix debug

--- matrix/spec/pmc_regression_4_spec.cr
+++ matrix/spec/pmc_regression_4_spec.cr
@@ -2,9 +2,25 @@ require "./spec_helper"
 
 describe Neuratron::DeepModel do
   it "with 4 exemples" do
-    model = Neuratron::DeepModel.new(2, 5, 1)
-    result = model.predict([1.0, 2.0])
-    pp "result", result
-    pp "model", model
+    # model = Neuratron::DeepModel.new(2, 5, 1)
+    # result = model.predict([1.0, 2.0])
+    # pp "result", result
+    # pp "model", model
+    inputs = [
+      [1.0, 2.0],
+      [3.0, 4.0],
+      [5.0, 5.0],
+      [6.0, 6.0],
+    ]
+
+    expected_outputs = [
+      [2.0],
+      [3.0],
+      [1.0],
+      [40.0],
+    ]
+
+    model = Neuratron::DeepModel.new(2,3,1)
+    model.train_regression(inputs, expected_outputs, 2, 0.4)
   end
 end
