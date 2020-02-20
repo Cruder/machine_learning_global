015cc966011dc57cab3c39ea38ae10c9fa9f1536
amelie-certin <amelie.certin@gmail.com>
2020-02-20 12:00:35 +0100

    Add wild function

--- matrix/src/matrix.cr
+++ matrix/src/matrix.cr
@@ -1,3 +1,16 @@
 require "aquaplot"
 require "./neuratron/lib"
 require "./neuratron/**"
+
+
+def max_indice(array : Array(Float64))
+  max = array[0];
+  index = 0;
+  array.each_with_index do |e, i|
+    if e > max
+      max = e
+      index = i
+    end
+  end
+  index
+end
