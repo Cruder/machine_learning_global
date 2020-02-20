ca2d3a5b8c6135299951baa24edb97705e8b7593
amelie-certin <amelie.certin@gmail.com>
2020-02-20 15:27:32 +0100

    Check invertible matrice

--- matrix/spec/linear_regression_4_spec.cr
+++ matrix/spec/linear_regression_4_spec.cr
@@ -6,13 +6,11 @@ describe Neuratron::LinearModel do
       [1.0, 2.0],
       [3.0, 4.0],
       [5.0, 5.0],
-      [6.0, 6.0],
     ]
     expected_outputs = [
       [2.0],
       [3.0],
       [1.0],
-      [40.0],
     ]
 
     inputs_to_classify = [
@@ -182,35 +180,37 @@ describe Neuratron::LinearModel do
 
 
     model = Neuratron::LinearModel.new(2, 1)
-    model.train(inputs.flatten, expected_outputs.flatten)
+    if model.train(inputs.flatten, expected_outputs.flatten)
+      predictions = inputs_to_classify.map do |input|
+        puts "Predict for #{input}"
+        results = model.predict(input)
+        puts "Prediction #{results}"
+        results
+      end
 
-    predictions = inputs_to_classify.map do |input|
-      puts "Predict for #{input}"
-      results = model.predict(input)
-      puts "Prediction #{results}"
-      results
+      positions = inputs_to_classify.zip(predictions).map do |data|
+        { data[0][0], data[0][1],  data[1] }
+      end
+
+      pp "positions", positions
+
+      math_formulat = "#{model.weights[0]} * 1 + #{model.weights[1]} * x + #{model.weights[2]} * y"
+      fns = [
+        AquaPlot::Scatter3D.from_points(positions).tap(&.set_title("Points")),
+        AquaPlot::Function.new("0", title: "0"),
+        AquaPlot::Function.new(math_formulat, title: math_formulat),
+      ]
+
+      pp fns[-1].style = "pm3d"
+
+      plt = AquaPlot::Plot3D.new fns
+      plt.set_key("left box")
+      plt.show
+      plt.set_view(100, 80, 1)
+      plt.show
+      plt.close
+    else
+      pp "Impossible de calculer un plan"
     end
-
-    positions = inputs_to_classify.zip(predictions).map do |data|
-      { data[0][0], data[0][1],  data[1] }
-    end
-
-    pp "positions", positions
-
-    math_formulat = "#{model.weights[0]} * 1 + #{model.weights[1]} * x + #{model.weights[2]} * y"
-    fns = [
-      AquaPlot::Scatter3D.from_points(positions).tap(&.set_title("Points")),
-      AquaPlot::Function.new("0", title: "0"),
-      AquaPlot::Function.new(math_formulat, title: math_formulat),
-    ]
-
-    pp fns[-1].style = "pm3d"
-
-    plt = AquaPlot::Plot3D.new fns
-    plt.set_key("left box")
-    plt.show
-    plt.set_view(100, 80, 1)
-    plt.show
-    plt.close
   end
 end
