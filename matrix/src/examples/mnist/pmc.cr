559fa56bffa8ed7e10b8ba3f8dd148a6643805ae
Alexandre Lairan <lairana@free.fr>
2020-02-21 13:59:20 +0100

    pmc read

--- matrix/src/examples/mnist/pmc.cr
+++ matrix/src/examples/mnist/pmc.cr
@@ -1,32 +1,55 @@
 require "../../matrix.cr"
 
-stdout = IO::Memory.new
-Process.run("ls ./data", shell: true, output: stdout)
-puts stdout
+# stdout = IO::Memory.new
+# Process.run("ls ./data", shell: true, output: stdout)
+# puts stdout
 
-images = File.open("./data/train-images-idx3-ubyte")
-labels = File.open("./data/train-labels-idx1-ubyte")
+# images = File.open("./data/train-images-idx3-ubyte")
+# labels = File.open("./data/train-labels-idx1-ubyte")
 
-loader = Mnist::Loader.new(images, labels)
-data = loader.call
+# loader = Mnist::Loader.new(images, labels)
+# data = loader.call
 
-images = data.map do |datum|
-  datum.image.map do |pixel|
-    clamp_value_rgb(pixel)
-  end
+# images = data.map do |datum|
+#   datum.image.map do |pixel|
+#     clamp_value_rgb(pixel)
+#   end
+# end
+
+# labels = data.map do |datum|
+#   Array.new(10) { |i| i == datum.label ? 1.0 : -1.0 }
+# end
+
+# pp images[0]
+# pp labels[0]
+
+# print_mnist_ascii(images[0])
+
+# model = Neuratron::DeepModel.new(28 * 28, 16, 10)
+
+# model.train_classification(images, labels, 5, 0.01)
+
+# model.save("./save/deep_mnist.json")
+
+
+class DeepModelData
+  JSON.mapping(
+    layer_count: Int32,
+    d: Array(Int32),
+    w: Array(Array(Array(Int32)))
+  )
 end
 
-labels = data.map do |datum|
-  Array.new(10) { |i| i == datum.label ? 1.0 : -1.0 }
-end
+json = File.read("./save/linear_model_mnist.json")
+data = DeepModelData.from_json(json)
 
-pp images[0]
-pp labels[0]
+raw_w = data.w.map do |i|
+  i.map do |j|
+    j.to_unsafe
+  end.to_unsafe
+end.to_unsafe
 
-print_mnist_ascii(images[0])
+pp raw_w
 
-model = Neuratron::DeepModel.new(28 * 28, 16, 10)
-
-model.train_classification(images, labels, 5, 0.01)
-
-model.save("./save/deep_mnist.json")
+pp data.w
+# raw_model = LibNeuratron::DeepModelData.new(size_input: data.input, size_output: data.output, inputs: data.weights.to_unsafe)
