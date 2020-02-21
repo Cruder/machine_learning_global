9854cb748bc474caf4c43f0a1a5ca7cc8f492d84
amelie-certin <amelie.certin@gmail.com>
2020-02-21 10:48:54 +0100

    Add gnuplot

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install -y curl
 curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
-sudo apt install -y crystal cmake libeigen3-dev
+sudo apt install -y crystal cmake libeigen3-dev gnuplot
 ./build.sh
